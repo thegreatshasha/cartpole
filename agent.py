6ec81ec819c8fcbf64a83e0d6e082d3b481bfe20
switchfootsid <siddharth481@gmail.com>
2016-04-10 13:16:27 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,9 +1,20 @@
-class     
+import numpy as np
+from code import 
+import random
+from code import * #Assuming states are defined in code - this should correspond to the environment or we have another file
+
+class Agent:
+    def __init__(self, force, timestep, epsilon, gamma):
+        self.t = timestep
+        self.epsilon = float(epsilon) #Greedy 
+        self.gamma = float(gamma) #Discount factor
+        self.Qvalues = {}
+        self.actions = np.arange(-force,force, 10) #Amount of horizontal force
     
     def update_Qvalue(self, pstate, action, state, reward):
-        max_qvalue = max([GameManager.get_Qvalue(self, state, a) for a in self.actions)])
+        max_qvalue = max([AgentG.get_Qvalue(self, state, a) for a in self.actions)])
         value = reward + self.gamma * max_qvalue
-        old_qvalue = GameChanger.Qvalues.get(str(pstate), str(action), None)
+        old_qvalue = Agent.Qvalues.get(str(pstate), str(action), None)
         
         if old_qvalue = None:
             self.Qvalues[(str(pstate), str(action))] = reward
@@ -14,7 +25,7 @@ class
         if random.random() <= self.epsilon:
             return self.actions[np.random.choice(len(self.actions))]
         else:
-            q = [GameManager.get_Qvalue(self, state, a) for a in self.actions]
+            q = [Agent.get_Qvalue(self, state, a) for a in self.actions]
             maxQ = max(q)                
             count = q.count(maxQ)
         
@@ -26,6 +37,13 @@ class
 
         action = self.actions[i]
         return action
+        
+    def get_actions(self):
             
     def get_Qvalue(self, state, action):
         return self.Qvalues.get(str(state), str(action), 0.0)
+    
+    def get_reward(self, state, action):
+        
+    
+    
