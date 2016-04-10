47ca60174abc44e0bb2156aeff1651e3808ad498
switchfootsid <siddharth481@gmail.com>
2016-04-10 13:29:01 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,49 +1,35 @@
 import numpy as np
 from code import 
-import random
-from code import * #Assuming states are defined in code - this should correspond to the environment or we have another file
 
 class Agent:
-    def __init__(self, force, timestep, epsilon, gamma):
+    def __init__(self, timestep, epsilon, gamma, training_steps):
         self.t = timestep
         self.epsilon = float(epsilon) #Greedy 
         self.gamma = float(gamma) #Discount factor
-        self.Qvalues = {}
-        self.actions = np.arange(-force,force, 10) #Amount of horizontal force
-    
-    def update_Qvalue(self, pstate, action, state, reward):
-        max_qvalue = max([AgentG.get_Qvalue(self, state, a) for a in self.actions)])
-        value = reward + self.gamma * max_qvalue
-        old_qvalue = Agent.Qvalues.get(str(pstate), str(action), None)
+        self.train_iter = training_steps
         
-        if old_qvalue = None:
-            self.Qvalues[(str(pstate), str(action))] = reward
-        else:
-            self.Qvalues[(str(pstate), str(action))] = reward + self.gamma * (max_qvalue - old_value)
-    
-    def choose_actions(self):
-        if random.random() <= self.epsilon:
-            return self.actions[np.random.choice(len(self.actions))]
-        else:
-            q = [Agent.get_Qvalue(self, state, a) for a in self.actions]
-            maxQ = max(q)                
-            count = q.count(maxQ)
-        
-        if count > 1:
-            best = [i for i in range(len(self.actions)) if q[i] == maxQ]
-            i = best[np.random.choice(len(best))]
-        else:
-            i = q.index(maxQ)
-
-        action = self.actions[i]
-        return action
-        
-    def get_actions(self):
-            
     def get_Qvalue(self, state, action):
-        return self.Qvalues.get(str(state), str(action), 0.0)
-    
-    def get_reward(self, state, action):
+        """
+        Should return Q(state, action)
+        """
+     
+    def getValue(self, pstate, action, state, reward):
+        """ What is the value of this state under the best action?
+            V(s) = max_{a in actions} Q(s,a)
+        """
+    def getPolicy(self, state):
+        """
+        What is the best action to take in the state.
+        policy(s) = arg_max_{a in actions} Q(s,a)
+        If multiple actions with same max Q-value, choose random.
+        """
+        
+    def choose_action(self):
+        """
+        Given a criteria, choose an actio from available action list and return it.
+        Exploration - Exploitation code here.
+        """
+        
         
     
     
