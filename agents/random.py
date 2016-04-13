3676843aa83ac8a06eeb03d543d9a0b9ec272fa0
Shashwat Kumar <thegreatshasha@gmail.com>
2016-04-14 00:21:20 +0530

    popo

---
+++ agents/random.py
@@ -0,0 +1,12 @@
+import numpy as np
+from abstract import AbstractAgent
+
+class RandomAgent(AbstractAgent):
+    def __init__(self, rngs):
+        self.actions = rngs[-1] # Last range is action range
+
+    def update_Qvalue(self, pstate, action, nstate, reward):
+        pass
+
+    def choose_action(self, state):
+        return self.actions[np.random.choice(len(self.actions)-1)] # Choose any element but last
